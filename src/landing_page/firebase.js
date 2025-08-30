5b5bfc604235270e0e24fc136e0c12ff087208d6
Afzal14786 <mdafzal14777@gmail.com>
2025-08-31 05:12:00 +0530

    fixing the firebase.js and encapsulate it

--- src/landing_page/firebase.js
+++ src/landing_page/firebase.js
@@ -1,14 +1,17 @@
 
 import { initializeApp } from "firebase/app";
 import {getAuth} from "firebase/auth";
+import dotenv from "dotenv"
+dotenv.config();
+
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDRJIS6oHqiaVeUk4Q9FSn4-SBF4nkmtC8",
-  authDomain: "zerodha-clone-5fb13.firebaseapp.com",
-  projectId: "zerodha-clone-5fb13",
-  storageBucket: "zerodha-clone-5fb13.firebasestorage.app",
-  messagingSenderId: "397825119882",
-  appId: "1:397825119882:web:c853f71d60c4b3f4823f73"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 };
 
 // Initialize Firebase
